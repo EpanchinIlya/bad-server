1247f6ddb1dde520b8cdc18fc05138eada18b0d7
Epanchin Ilya <epnchin.ilya@yandex.ru>
2025-06-02 10:33:38 +0500

    fix getOrders

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -16,9 +16,9 @@ import { Role } from '../models/user'
 
 
 const orderRouter = Router()
-
+// orderValidation.getAllOrders,//
 orderRouter.post('/', auth, validateOrderBody, createOrder)
-orderRouter.get('/all', auth, orderValidation.getAllOrders, getOrders)
+orderRouter.get('/all', auth, orderValidation.getAllOrders,  roleGuardMiddleware(Role.Admin),  getOrders)
 orderRouter.get('/all/me', auth, orderValidation.getOrdersCurrentUser, getOrdersCurrentUser)
 orderRouter.get('/:orderNumber', auth, orderValidation.getOrderByNumber, roleGuardMiddleware(Role.Admin), getOrderByNumber)
 orderRouter.get('/me/:orderNumber', auth, orderValidation.getOrderByNumberCurrentUser, getOrderCurrentUserByNumber)
